61b6748ebe3fa99d0a9678ad7047fb2d665b762d
FS-G <71540465+FS-G@users.noreply.github.com>
2025-01-10 13:38:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.documents import Document  
+import pinecone
 
 
 # for the  code  file 
